8aeb162edfc7cd85beb05c941692c36445681b63
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-12-02 15:24:53 -1000

    implemented google maps and also added marker

--- src/components/MainView/Home.js
+++ src/components/MainView/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableHighlight, Image, KeyboardAvoidingView } from 'react-native';
+import MapView from 'react-native-maps';
 
 
 export default class Home extends Component {
@@ -7,9 +8,25 @@ export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text>
-        HEUUUUYYYY!!!!
-      </Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 21.3347,
+            longitude: -158.0808,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+            <MapView.Marker
+              coordinate={{
+                latitude: 21.3347,
+                longitude: -158.0808,
+              }}>
+                <View style={styles.radius}>
+                  <View style={styles.marker} />
+                </View>
+            </MapView.Marker>
+
+        </MapView>
       </View>
     );
   }
@@ -19,6 +36,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#3498db'
+  },
+  map: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute'
+  },
+  radius: {
+    height: 20,
+    width: 20,
+    borderRadius: 50/2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 255, 117, 0.75)',
+    borderWidth: 1,
+    borderColor: 'rgba(0,122,255,0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker: {
+
   }
 
 });

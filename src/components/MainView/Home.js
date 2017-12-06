85dc2e3570ba1726a65f80f5b888f031ff5fcc4c
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-12-05 18:35:48 -1000

    made input boxes dynamic

--- src/components/MainView/Home.js
+++ src/components/MainView/Home.js
@@ -1,96 +1,99 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableHighlight, Image, KeyboardAvoidingView, Dimensions} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity, Image, KeyboardAvoidingView, Dimensions} from 'react-native';
 import MapView from 'react-native-maps';
+import treelogo from '../../images/treelogo.jpg';
+
 
 const {width, height} =Dimensions.get('window')
 
-const SCREEN_HEIGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE = 21.34031;
+const LONGITUDE = -158.06647;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
 
 export default class Home extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state ={
-      initialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitudeDelta: 0
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
       },
-      markerPosition: {
-        latitude: 0,
-        longitude: 0
-      }
-    }
+      markers: [],
+    };
+
+    this.onMapPress = this.onMapPress.bind(this);
   }
 
-  watchID: ?number = null
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-
-      this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
-
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var lastRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-
-      this.setState({initialPosition: lastRegion})
-      this.setState({markerPosition: lastRegion})
-    })
-  }
-
-  componentWillUnmount(){
-    navigator.geolocation.clearWatch(this.watchID)
+  onMapPress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: `Tree${id++}`,
+        },
+      ],
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <MapView
+          provider={this.props.provider}
           style={styles.map}
-          region={this.state.initialPosition}>
+          initialRegion={this.state.region}
+          onPress={this.onMapPress}
+        >
+          {this.state.markers.map(marker => (
             <MapView.Marker
-              coordinate={this.state.markerPosition}>
-                <View style={styles.radius}>
-                  <View style={styles.marker} />
-                </View>
+              title={marker.key}
+              key={marker.key}
+              coordinate={marker.coordinate}>
+ <MapView.Callout>
+  <Text>
+    {marker.key}
+      </Text>
+    <TextInput
+      placeholder="Description"
+      style={styles.inputMap}
+    />
+
+ </MapView.Callout>
             </MapView.Marker>
 
+          ))}
+          <View style={styles.radius}>
+                 <View style={styles.marker} />
+               </View>
         </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.setState({ markers: [] })}
+            style={styles.bubble}
+          >
+            <Text>Delete all created Markers </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#3498db'
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
     left: 0,
@@ -99,25 +102,50 @@ const styles = StyleSheet.create({
     bottom: 0,
     position: 'absolute'
   },
-  radius: {
-    height: 40,
-    width: 40,
-    borderRadius: 50/2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0,122,255,0.3)',
-    borderWidth: 1,
-    borderColor: 'rgba(0,122,255,0.6)',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.9)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
   },
-  marker: {
-    height: 20,
-    width: 20,
-    borderRadius: 20/2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0, 255, 117, 0.75)',
-    borderWidth: 3,
-    borderColor: 'rgb(147, 84, 0)',
-  }
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+  inputMap: {
+    height: 40,
+    width: 80,
 
+  },
+  radius: {
+  height: 40,
+  width: 40,
+  borderRadius: 50/2,
+  overflow: 'hidden',
+  backgroundColor: 'rgba(0,122,255,0.3)',
+  borderWidth: 1,
+  borderColor: 'rgba(0,122,255,0.6)',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+marker: {
+  height: 20,
+  width: 20,
+  borderRadius: 20/2,
+  overflow: 'hidden',
+  backgroundColor: 'rgba(0, 255, 117, 0.75)',
+  borderWidth: 3,
+  borderColor: 'rgb(147, 84, 0)',
+}
 });

c2766822f8d9db34d0e4787fd3e99d3cd7dd3473
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-12-06 16:36:07 -1000

    styled Home page and added descriptions

--- src/components/MainView/Home.js
+++ src/components/MainView/Home.js
@@ -4,30 +4,55 @@ import { StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity
 
 export default function Home({ navigation }) {
   return (
-    <View style={styles.container}>
+  <View style={styles.container}>
 
-      <Text>This is another page after the home page</Text>
-
+    <View style={styles.topcontainer}>
       <TouchableOpacity onPress={() => navigation.navigate('NewPage')}
         style={styles.buttonContainer}>
         <Text style={styles.buttonText}>Find Trees</Text>
       </TouchableOpacity>
-
+        <Text>
+          ADD SOME TYPE OF DESCRIPTION HERE EXPLAINING WHAT THIS BUTTON DOES AND WHERE IT ROUTES YOU NEXT ALSO
+          ADD A BACKGROUND IMAGE OR DECIDE IF YOU WANT TO MAKE IT LOOK LIKE THIS COOL STUFF!
+        </Text>
+    </View>
+    <Text style={styles.middleThing}>ADD SOMETHING HERE (LOGO)</Text>
+    <View style={styles.bottomcontainer}>
       <TouchableOpacity onPress={() => navigation.navigate('Create')}
         style={styles.buttonContainer}>
         <Text style={styles.buttonText}>Create Project</Text>
       </TouchableOpacity>
-
+        <Text>
+          ADD SOME TYPE OF DESCRIPTION HERE EXPLAINING WHAT THIS BUTTON DOES AND WHERE IT ROUTES YOU NEXT ALSO
+          ADD A BACKGROUND IMAGE OR DECIDE IF YOU WANT TO MAKE IT LOOK LIKE THIS COOL STUFF!
+        </Text>
     </View>
+
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+
+  },
+  topcontainer: {
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+    flex: .5,
+    padding: 20
+  },
+  middleThing: {
+   textAlign: 'center',
+   backgroundColor: 'mediumaquamarine'
+ },
+  bottomcontainer: {
+    justifyContent: 'center',
     backgroundColor: '#3498db',
     padding: 20,
-    justifyContent: 'center'
+    flex: .5
   },
   buttonContainer: {
     backgroundColor: 'rgb(18, 130, 44)',
